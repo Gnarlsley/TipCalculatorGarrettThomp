80a4332074806ca31457ed754727e726a67f8a76
gnarlsley <thompson.garrett7413@gmail.com>
2024-01-16 13:00:23 -0600

    Added folders for MVC format, connected layout, created model and method, adding elements to index view

---
+++ TipCalculatorGarrettThomp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TipCalculatorGarrettThomp.Models;
+
+namespace TipCalculatorGarrettThomp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.tipAmount = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(TipInput tip)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.tipAmount = tip.CalculateTip();
+            }
+            else
+            {
+                ViewBag.tipAmount = "";
+            }
+            return View();
+        }
+    }
+}
---
+++ TipCalculatorGarrettThomp/Models/TipInput.cs
@@ -0,0 +1,19 @@
+﻿namespace TipCalculatorGarrettThomp.Models
+{
+    public class TipInput
+    {
+        public double MealCost { get; set; }
+
+        public double[] CalculateTip()
+        {
+            double cost = MealCost;
+            double tipFifteen = cost * .15;
+            double tipTwenty = cost * .2;
+            double tipTwentyFive = cost * .25;
+            double[] tipAmounts = {tipFifteen, tipTwenty, tipTwentyFive};
+            return tipAmounts;
+        }
+
+
+    }
+}
